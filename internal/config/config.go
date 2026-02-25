de7d02f869eb5870f19eaa6a7f981f07a8882e34
10xdev4u-alt <10xdev4u@gmail.com>
2026-02-25 23:40:40 +0530

    feat: internal/config implementation

---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig `mapstructure:"server"`
+	Log    LogConfig    `mapstructure:"log"`
+}
+
+type ServerConfig struct {
+	Port int `mapstructure:"port"`
+}
+
+type LogConfig struct {
+	Level string `mapstructure:"level"`
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.SetDefault("server.port", 8080)
+	viper.SetDefault("log.level", "info")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
